b0e337d9c7bb765779d3855a94f392921840e475
bakasub <flannoblesse@gmail.com>
2022-08-04 14:50:54 +0700

    bai tap

---
+++ JS practice.js
@@ -0,0 +1,9 @@
+function click1(){
+    alert(1);
+}
+function mouseover() {
+    document.getElementById("sentence1").style.color = "red";
+}
+function mouseout() {
+    document.getElementById("sentence1").style.color = "black";
+}
